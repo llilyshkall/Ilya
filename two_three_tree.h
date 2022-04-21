a43f75b6366a8462dedcf7712c94e82faac42de2
llilyshkall <104125086+llilyshkall@users.noreply.github.com>
2022-04-21 22:30:41 +0300

    Update two_three_tree.h

--- two_three_tree.h
+++ two_three_tree.h
@@ -1,8 +1,12 @@
 #ifndef __two_three_tree_h
 #define __two_three_tree_h
 
-
 #define two_three_tree node*
-//functions
 
+int TTT_find(two_three_tree T);
+// поиск
+void TTT_push(two_three_tree T, int x);
+// добавление с балансировкой
+void TTT_pop(two_three_tree T, int x);
+// удаление с балансировкой
 #endif
