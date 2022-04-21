0b57205e306b21ccc274be9b0bfe9f154f01361c
llilyshkall <104125086+llilyshkall@users.noreply.github.com>
2022-04-21 22:25:06 +0300

    Update two_three_tree.c

--- two_three_tree.c
+++ two_three_tree.c
@@ -1,2 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+struct node
+{
+    int size;      // количество занятых ключей
+    int key[3];    
+    node *first;   // *first <= key[0];
+    node *second;  // key[0] <= *second < key[1];
+    node *third;   // key[1] <= *third < key[2];
+    node *fourth;  // kye[2] <= *fourth.
+    node *parent;
+}
+
+
+int TTT_find(two_three_tree T)
+{
+
+}
+
+void TTT_push(two_three_tree T, int x)
+{
+  
+}
+
+void TTT_pop(two_three_tree T, int x)
+{
+  
+}
