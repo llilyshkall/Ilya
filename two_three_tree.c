00bb9a39467aefae0bc2d0bdadb4039c9f83b3f1
llilyshkall <104125086+llilyshkall@users.noreply.github.com>
2022-05-02 21:58:52 +0300

    Update two_three_tree.c

--- two_three_tree.c
+++ two_three_tree.c
@@ -13,38 +13,19 @@ struct node
 }
 
 
-int TTT_find(two_three_tree T)
+int TTT_find(two_three_tree T, int x)
 {
 
 }
 
-void TTT_push(two_three_tree T, int x)
+int TTT_push(two_three_tree T, int x)
 {
 	printf("test\n");
 }
 
-void TTT_pop(two_three_tree T, int x)
+int TTT_pop(two_three_tree T, int x)
 {
   
 }
 
 
-void TTT_clear(two_three_tree T)
-{
-    if(first != NULL)
-    {
-        TTT_clear(T->first);
-        free(T->first);
-    }
-    if(second != NULL)
-    {
-        TTT_clear(T->second);
-        free(T->second);
-    }
-    if(third != NULL)
-    {
-        TTT_clear(T->third);
-        free(T->third);
-    }
-    free(T);
-}
